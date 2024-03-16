950de647c88c687e3c2ea28a9aa1be00f354417d
ahed666 <ahed0naser0eddin@gmail.com>
2024-03-16 15:05:42 +0400

    edit 3/16

--- resources/views/livewire/questions-statistics.blade.php
+++ resources/views/livewire/questions-statistics.blade.php
@@ -107,7 +107,7 @@
         {{-- @foreach($formquestions as $key => $question) --}}
         <div id="question_template" wire:loading wire:loading.class="opacity-50" wire:target="currentQuestion" class="grid grid-cols-12  bg-white my-1 rounded-[0.5rem] border-[1px] border-gray-300  p-3 ">
 
-            <div id="loading-animation" wire:loading wire:loading.class.remove="hidden" wire:loading wire:loading.class="flex" wire:target="currentQuestion" class="hidden left-1/2 top-1/2  ">
+            <div id="loading-animation" wire:loading wire:loading.class.remove="hidden" wire:loading wire:loading.class="flex" wire:target="currentQuestion" class="hidden absolute left-1/2 top-1/2  ">
                 <svg class="animate-spin h-10 w-10 mr-1 text-secondary_blue" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"></path>
                 </svg>
