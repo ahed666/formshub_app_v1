d01bfb63d898110e50e605f47debd514020173b3
ahed666 <ahed0naser0eddin@gmail.com>
2024-04-20 17:01:01 +0400

    edit 4/20

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,6 +51,9 @@
 <html  lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+        <?php
+    echo '<?xml version="1.0" encoding="utf-8"?>';
+    ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
